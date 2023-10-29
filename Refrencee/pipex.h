78e93879c85a6aee51a27f0176adfcb0f6024020
MShaxaib <139437420+MShaxaib@users.noreply.github.com>
2023-10-29 13:54:05 +0000

    pipex

--- Refrencee/pipex.h
+++ Refrencee/pipex.h
@@ -21,6 +21,18 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+char	**ft_split(const char *s, char c);
+int		ft_strlen(const char *str);
+char	*ft_strnstr(const char *haystack, const char *needle, int len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_path(char *cmd, char **env);
+char	*ft_pwd_path(char *cmd, char **env);
+void	ft_get_cmd(char *argv, char **env);
+void	ft_parent_process(char **argv, char **env, int *fd);
+void	ft_child_process(char **argv, char **env, int *fd);
+// int		main(int argc, char **argv, char **env);
+
+
 
 #endif
 
