4238084bcb468a08808048a9bb41ef8059cb5bd3
MShaxaib <139437420+MShaxaib@users.noreply.github.com>
2023-12-04 08:49:01 +0000

    fix day 2

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -31,32 +31,35 @@ void	close_pipes(t_pipex *pipex)
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	// t_pipex	pipex;
+	t_pipex	pipex;
 
-	if (argc != 5)
-		return (msg(ERR_INPUT));
-	(void) argv;
-	while (*envp)
-		printf("%s\n", *envp++);
-	// pipex.infile = open(argv[1], O_RDONLY);
-	// if (pipex.infile < 0)
-	// 	return (msg(ERR_INFILE));
-	// pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
-	// if (pipex.outfile < 0)
-	// 	return (msg(ERR_OUTFILE));
-	// if (pipe(pipex.tube) < 0)
-	// 	return (msg(ERR_PIPE));
-	// pipex.paths = find_path(envp);
-	// pipex.cmd_paths = ft_split(pipex.paths, ':');
-	// pipex.pid1 = fork();
-	// if (pipex.pid1 == 0)
-	// 	first_child(pipex, argv, envp);
-	// pipex.pid2 = fork();
-	// if (pipex.pid2 == 0)
-	// 	second_child(pipex, argv, envp);
-	// close_pipes(&pipex);
-	// waitpid(pipex.pid1, NULL, 0);
-	// waitpid(pipex.pid2, NULL, 0);
-	// parent_free(&pipex);
+	// if (argc != 5)
+	// 	return (msg(ERR_INPUT));
+	// (void) argv;
+	// while (*envp)
+	// 	printf("%s\n", *envp++);
+
+
+
+	pipex.infile = open(argv[1], O_RDONLY);
+	if (pipex.infile < 0)
+		return (msg(ERR_INFILE));
+	pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
+	if (pipex.outfile < 0)
+		return (msg(ERR_OUTFILE));
+	if (pipe(pipex.tube) < 0)
+		return (msg(ERR_PIPE));
+	pipex.paths = find_path(envp);
+	pipex.cmd_paths = ft_split(pipex.paths, ':');
+	pipex.pid1 = fork();
+	if (pipex.pid1 == 0)
+		first_child(pipex, argv, envp);
+	pipex.pid2 = fork();
+	if (pipex.pid2 == 0)
+		second_child(pipex, argv, envp);
+	close_pipes(&pipex);
+	waitpid(pipex.pid1, NULL, 0);
+	waitpid(pipex.pid2, NULL, 0);
+	parent_free(&pipex);
 	return (0);
 }
